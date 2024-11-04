cf19391abc57e96ad54a7f1d9aada60b770ca28f
FranciscoLattaro <lpdcmelo@gmail.com>
2024-11-04 16:05:30 -0300

    .

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("C:\\Users\\e862291\\Desktop\\GitHub\\Accesa\\Accesa-Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\e862291\\Desktop\\GitHub\\Accesa\\Accesa-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\e862291\\Desktop\\GitHub\\Accesa\\Accesa-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\e862291\\Desktop\\GitHub\\Accesa\\Accesa-Docs\\src\\css\\custom.css"),
+  require("C:\\Users\\e862291\\Desktop\\Prueba\\prueba\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\e862291\\Desktop\\Prueba\\prueba\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\e862291\\Desktop\\Prueba\\prueba\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\e862291\\Desktop\\Prueba\\prueba\\src\\css\\custom.css"),
 ];
